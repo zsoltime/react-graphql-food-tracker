4d4a725b3f18579d4a828c2f7d9479e399fc8680
Zsolt Meszaros <zsolt@sowad.com>
2018-11-22 23:18:28 +0000

    Add index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Intro = styled.div`
+  text-align: center;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 300;
+    margin: 0 auto 1rem;
+  }
+`;
 
 const Home = () => (
-  <div>
-    <p>Food tracker</p>
+  <Intro>
+    <h1>Food Tracker App</h1>
     <img src="/static/favicon.png" alt="" />
-  </div>
+  </Intro>
 );
 
 export default Home;
