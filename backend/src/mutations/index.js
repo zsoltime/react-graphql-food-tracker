da8851d0b6c15b849a70b8bbcdc49b4c142bb9cc
Zsolt Meszaros <zsolt@sowad.com>
2018-11-23 21:44:43 +0000

    Add sign out mutation

--- backend/src/mutations/index.js
+++ backend/src/mutations/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const signIn = require('./user/signIn');
+const signOut = require('./user/signOut');
 const signUp = require('./user/signUp');
 
 module.exports = {
   signIn,
+  signOut,
   signUp,
 };
