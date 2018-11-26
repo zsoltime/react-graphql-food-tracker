99c0ad308d91d0f5f3c9c074235888ce71ae684e
Zsolt Meszaros <zsolt@sowad.com>
2018-11-26 11:28:47 +0000

    Send email from updateEmail mutation

--- backend/src/mutations/user/updateEmail.js
+++ backend/src/mutations/user/updateEmail.js
@@ -3,6 +3,8 @@
 const isEmail = require('validator/lib/isEmail');
 const nanoid = require('nanoid');
 
+const { transport } = require('../../mail');
+
 module.exports = async (_, args, ctx) => {
   if (!ctx.request.userId) {
     throw new Error(
@@ -28,12 +30,48 @@ module.exports = async (_, args, ctx) => {
   const tokenExpiry = Date.now() + 60 * 60 * 1000; // 1hour
 
   // set user a pending email
-  const res = await ctx.db.mutation.updateUser({
+  await ctx.db.mutation.updateUser({
     data: { pendingEmail, token, tokenExpiry },
     where: { id: ctx.request.userId },
   });
 
-  // TODO: Send validation email
+  await transport
+    .sendMail({
+      from: 'Food Tracker Team <noreply@foodtracker.com>',
+      to: pendingEmail,
+      subject: 'Confirm Your New Email Address',
+      html: `
+      <div style="
+        border: 1px solid #444;
+        color: #444;
+        font-family: sans-serif;
+        font-size: 1.25rem;
+        font-weight: 300;
+        line-height: 1.5;
+        padding: 1.25rem;
+      ">
+        <h2>Hello There!</h2>
+        <p>You recently requested to change your email. Use the button below to confirm your new email address.</p>
+        <p><a style="
+        background-color: #eb5190;
+        border-radius: 2px;
+        color: #fff;
+        display: block;
+        font-size: 1.5rem;
+        margin:1rem auto;
+        padding: 0.5rem 2rem;
+        text-align:center;
+        text-decoration: none
+        " href="${
+  process.env.FRONTEND_URL
+}/updateEmail?token=${token}">Click here</a></p>
+        <p>All the best, <br>Food Tracker Team</p>
+      </div>
+    `,
+    })
+    .catch(() => {
+      // throw new Error('Error sending email. Please try again later.');
+    });
 
   return { message: 'Email with token sent' };
 };
