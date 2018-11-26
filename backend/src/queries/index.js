1b73359093604eeb41f37970e8fd3c5958fb88ef
Zsolt Meszaros <zsolt@sowad.com>
2018-11-26 09:12:13 +0000

    Fix: listUsers query
    Forgot to commit the index file :(

--- backend/src/queries/index.js
+++ backend/src/queries/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const me = require('./users/me');
+const listUsers = require('./users/listUsers');
 const user = require('./user');
 
 module.exports = {
+  listUsers,
   me,
   user,
 };
