21b3e748ccdf2b8452e637cd3e6adbdfe4fa6163
Zsolt Meszaros <zsolt@sowad.com>
2018-11-26 15:31:53 +0000

    Update resetPassword tests

--- backend/src/mutations/user/resetPassword.test.js
+++ backend/src/mutations/user/resetPassword.test.js
@@ -3,6 +3,47 @@
 const resetPassword = require('./resetPassword');
 
 describe('resetPassword mutation', () => {
+  it('updates user password and updates cookie', async () => {
+    const args = { password: 'test1234', token: 'validToken' };
+    const fakeUser = {
+      id: 'testId',
+      email: 'test@email.io',
+      pendingEmail: null,
+      permissions: ['USER'],
+      profile: null,
+      token: args.token,
+      tokenExpiry: 1234567890,
+    };
+    const mockedContext = {
+      db: {
+        mutation: {
+          updateUser: jest.fn().mockImplementation(() => Promise.resolve({
+            ...fakeUser,
+            token: null,
+            tokenExpiry: null,
+          })),
+        },
+        query: {
+          users: jest
+            .fn()
+            .mockImplementation(() => Promise.resolve([fakeUser])),
+        },
+      },
+      response: { cookie: jest.fn() },
+    };
+
+    const user = await resetPassword(null, args, mockedContext);
+
+    expect(mockedContext.response.cookie).toHaveBeenCalled();
+    expect(
+      mockedContext.response.cookie.mock.calls[0]
+    ).toMatchSnapshot();
+    expect(
+      mockedContext.db.mutation.updateUser.mock.calls[0]
+    ).toMatchSnapshot();
+    expect(user).toMatchSnapshot();
+  });
+
   it('throws error if token is not valid', () => {
     const args = { password: 'test1234', token: 'invalidToken' };
     const mockedContext = {
